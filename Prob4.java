1ee009e6c6e371471f0b4546ea1dbb591e5af1ed
Anurup <anurupkoli01@gmail.com>
2023-10-01 14:49:17 +0530

    Problem4

---
+++ Prob4.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Prob4 {
+ public static void main(String[] args) {
+
+
+  try {
+   Scanner in = new Scanner(System.in);
+   int n = in .nextInt();
+   in.close();
+   //String s=???; Complete this line below
+    String s = Integer.toString(n);
+   //Write your code here
+
+   
+   if (n == Integer.parseInt(s)) {
+    System.out.println("Good job");
+   } else {
+    System.out.println("Wrong answer.");
+   }
+  } catch (Exception e) {
+   System.out.println(e);
+  }
+ }
+}
